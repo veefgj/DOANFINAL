a8d6dbcb524b0a7eeb6e6bfa9b898f1ac9025415
veefgj <veefgjit@gmail.com>
2024-04-28 16:45:36 +0700

    update vnpay

---
+++ PC_Server/datn-be/src/main/java/com/poly/datn/be/config/DTO/thongKeTheoThang.java
@@ -0,0 +1,14 @@
+package com.poly.datn.be.config.DTO;
+
+import lombok.Data;
+
+@Data
+public class thongKeTheoThang {
+    String productId;
+
+    String productName;
+
+    String imageName;
+
+    String totalQuantity;
+}
